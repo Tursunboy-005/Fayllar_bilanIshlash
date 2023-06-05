d4a508b7cf7d3e5adc6e2128eecf711b9f21d2aa
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-05 09:53:01 +0500

    Initial commit

---
+++ Fayllar_bilanIshlasj/Fayllar_bilanIshlasj/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fayllar_bilanIshlasj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
