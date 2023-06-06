e6b97fc3b61ab4a2b79db60db43ff9f532bfc3bf
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-06 23:38:46 +0500

    Task_1 borligini tekshirish

--- Fayllar_bilanIshlasj/Fayllar_bilanIshlasj/Program.cs
+++ Fayllar_bilanIshlasj/Fayllar_bilanIshlasj/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string path = "C:\\Games";
+
+            string result = CheckPathBorligi(path);
+            Console.WriteLine(result);
+        }
+
+        public static string CheckPathBorligi(string path)
+        {
+            if(Directory.Exists(path))
+            {
+                string[] directories = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+
+                if(directories.Length > 0 && files.Length > 0)
+                {
+                    return "Papka va fayllar bor";
+                }
+                else if(directories.Length > 0)
+                {
+                    return "Papkalar bor;";
+                }else if(files.Length > 0)
+                {
+                    return "Fayllar bor";
+                }
+                else
+                {
+                    return " Papka bo'sh";
+                }
+            }
+            else
+            {
+                return "Berilgan Path yoq";
+            }
         }
     }
 }
