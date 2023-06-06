7f2e314fbc9e2068611ace06e031e8eb13ef9357
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-06-06 23:23:56 +0500

    Lesson repeat

--- Fayllar_bilanIshlasj/Fayllar_bilanIshlasj/Program.cs
+++ Fayllar_bilanIshlasj/Fayllar_bilanIshlasj/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-             
+            var parentDirectory = Directory.GetParent("C:\\games");
+            Console.WriteLine(parentDirectory);
+
+            string path = "C:\\New folder\\";
+
+            if(Directory.Exists(path))
+            {
+                var directories = Directory.GetDirectories(path);
+                string newDirectory = "C:\\New folder\\";
+
+
+                foreach(var d in directories)
+                {
+                    Console.WriteLine(d);
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            DateTime dateTime = Directory.GetLastWriteTime(path);
+            Directory.Delete(path, true);
+            Directory.Delete("C:\\New folder", true);
         }
     }
 }
